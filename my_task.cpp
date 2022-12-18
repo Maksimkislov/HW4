57cd261aabc4b56717b12f68a7799a5a4f8c51a8
Maksimkislov <44447617+Maksimkislov@users.noreply.github.com>
2022-12-18 23:24:50 +0300

    Add files via upload
    34. Задача для агронома. Председатель дачного кооператива Сидоров В.И.
    получил указание, что в связи с составлением единого земельного кадастра,
    необходимо представить справку о площади занимаемых земель. Известно,
    что территория с запада и востока параллельна меридианам, на севере ограничены параллелью, а с юга выходят к реке, описываемой функцией f(x).
    Требуется создать многопоточное приложение, вычисляющее площадь
    угодий методом адаптивной квадратуры. При решении использовать парадигму рекурсивного параллелизма. Замечание: кривизну Земли из-за малой
    занимаемой площади не учитывать.

---
+++ my_task.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <pthread.h>
+
+#define TREAD_NUM 8
+
+pthread_mutex_t mutexFuel;
+
+double res = 0;
+std::vector<double>coef(3);
+std::vector<int> pow(3);
+int n = 3;
+double west = 0, east = 0, start = 0, finish = 0, size = 0.01;
+
+void* count_space(void* args) {
+    pthread_mutex_lock(&mutexFuel);
+    pthread_mutex_lock(&mutexFuel);
+    for (double i = start; i < finish; i += size) {
+        double height = 0;
+        for (size_t j = 0; j < coef.size(); ++j) {
+            if (pow[j] == 2) {
+                height += coef[j] * i * i;
+            } 
+            if (pow[j] == 1) {
+                height += coef[j] * i;
+            }
+            if (pow[j] == 0) {
+                height += coef[j];
+            }           
+        }
+        std::cout << i << "\n";
+        // std::cout << (l - height) << " ";
+        res += (west - height) * size;
+    }
+    pthread_mutex_unlock(&mutexFuel);
+    pthread_mutex_unlock(&mutexFuel);
+}
+
+
+int main(){
+    double west, east, north;
+    std::cout << "Write length of west, east and north border:\n";
+    std::cin >> west >> east >> north;
+    // We will think that f(x) is a ratioanal function 
+    // Where the main element is in the power of n
+
+    std::cout << "Write n: "<< "\n";
+    int n;
+    std::cin >> n;
+    std::vector<double> coef(n + 1);
+    std::vector<int> pow(n + 1);
+    std::cout << "Write (n + 1) coefficients: "<< "\n";
+    for (size_t i = 0; i < n + 1; ++i) {
+        std::cin >> coef[i];
+    }
+    for (size_t i = 0; i < n + 1; ++i) {
+        pow[i] = (n - 1 -i);
+    }
+    std::cout << "\n";
+    std::cout << "result: \n";
+    double size = 0.0001;
+
+
+    pthread_t th[TREAD_NUM];
+    pthread_mutexattr_t recursiveMutexAttributes;
+    pthread_mutexattr_init(&recursiveMutexAttributes);
+    pthread_mutexattr_settype(&recursiveMutexAttributes, PTHREAD_MUTEX_RECURSIVE);
+    pthread_mutex_init(&mutexFuel, &recursiveMutexAttributes);
+
+    int i;
+    for (int i = 0; i < TREAD_NUM; ++i) {
+        if (pthread_create(&th[i], NULL, count_space, NULL) != 0) {
+            perror("Failed to create thread");
+        }
+    }
+
+    
+    std::cout << res << "\n";
+    
+    pthread_mutexattr_destroy(&recursiveMutexAttributes);
+    pthread_mutex_destroy(&mutexFuel);
+
+    return 0;
+}
