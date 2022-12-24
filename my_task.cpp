8df0864791e048940a165c5166948f71bd85dd4c
Maksimkislov <44447617+Maksimkislov@users.noreply.github.com>
2022-12-25 02:58:28 +0300

    Update my_task.cpp

--- my_task.cpp
+++ my_task.cpp
@@ -7,17 +7,17 @@
 pthread_mutex_t mutexFuel;
 
 double res = 0;
-std::vector<double>coef(3);
-std::vector<int> pow(3);
+std::vector<double>coef(3); // Вектор с коэффициентами, стоящими перед x^2, x, 1.
+std::vector<int> pow(3);    // Вектор со степенями pow = {2, 1, 0}
 int n = 3;
-double west = 0, east = 0, start = 0, finish = 0, size = 0.01;
+double west = 0, east = 0, start = 0, finish = 0, size = 0.01; // Инициализируем переменные
 
 void* count_space(void* args) {
-    pthread_mutex_lock(&mutexFuel);
+    pthread_mutex_lock(&mutexFuel); // Закрываем два мьютекса
     pthread_mutex_lock(&mutexFuel);
     for (double i = start; i < finish; i += size) {
         double height = 0;
-        for (size_t j = 0; j < coef.size(); ++j) {
+        for (size_t j = 0; j < coef.size(); ++j) { // Вычисляем значение квадратичной функции в конкретной точке
             if (pow[j] == 2) {
                 height += coef[j] * i * i;
             } 
@@ -30,9 +30,9 @@ void* count_space(void* args) {
         }
         std::cout << i << "\n";
         // std::cout << (l - height) << " ";
-        res += (west - height) * size;
+        res += (west - height) * size; // Находим площадь маленького прямоугольника и складываем найденную площадь со всей площадью
     }
-    pthread_mutex_unlock(&mutexFuel);
+    pthread_mutex_unlock(&mutexFuel); // Открываем два мьютекса
     pthread_mutex_unlock(&mutexFuel);
 }
 
